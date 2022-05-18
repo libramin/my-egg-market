c5490bcfcb7fbac5191ab3b393215cefafbbb40d
libramin <libramin9@gmail.com>
2022-05-18 18:57:38 +0900

    appBarColor_accordingToScroll

--- lib/screens/item/item_detail_screen.dart
+++ lib/screens/item/item_detail_screen.dart
@@ -14,10 +14,39 @@ class ItemDetailScreen extends StatefulWidget {
 
 class _ItemDetailScreenState extends State<ItemDetailScreen> {
   PageController _pageController = PageController();
+  ScrollController _scrollController = ScrollController();
+
+  Size? _size;
+  num? _statusBarHeight;
+  bool isAppbarCollapsed = false;
+
+  @override
+  void initState() {
+    _scrollController.addListener(() {
+      if(_size ==null || _statusBarHeight == null)
+        return ;
+      if(isAppbarCollapsed){
+        if(_scrollController.offset < _size!.width-kToolbarHeight-_statusBarHeight!){
+          isAppbarCollapsed = false;
+          setState(() {
+          });
+        }
+      }else{
+        if(_scrollController.offset > _size!.width-kToolbarHeight-_statusBarHeight!){
+          isAppbarCollapsed = true;
+          setState(() {
+
+          });
+        }
+      }
+    });
+    super.initState();
+  }
 
   @override
   void dispose() {
     _pageController.dispose();
+    _scrollController.dispose();
     super.dispose();
   }
 
@@ -26,52 +55,69 @@ class _ItemDetailScreenState extends State<ItemDetailScreen> {
     return FutureBuilder<ItemModel>(
         future: ItemService().getItem(widget.itemKey),
         builder: (context, snapshot) {
-          print(snapshot.data.toString());
           if (snapshot.hasData) {
             ItemModel itemModel = snapshot.data!;
             return LayoutBuilder(
               builder: (BuildContext context, BoxConstraints constraints) {
-                Size _size = MediaQuery.of(context).size;
-                return Scaffold(
-                  body: CustomScrollView(
-                    slivers: [
-                      SliverAppBar(
-                        expandedHeight: _size.width,
-                        pinned: true,
-                        flexibleSpace: FlexibleSpaceBar(
-                          centerTitle: true,
-                          title: SmoothPageIndicator(
-                              controller: _pageController, // PageController
-                              count: itemModel.imageDownUrl.length,
-                              effect: WormEffect(
-                                  activeDotColor: Theme.of(context)
-                                      .primaryColor,
-                              dotHeight: 10,
-                              dotWidth: 10), // your preferred effect
-                              onDotClicked: (index) {}),
-                          background: PageView.builder(
-                            controller: _pageController,
-                            allowImplicitScrolling: true,
-                            itemBuilder: (BuildContext context, int index) {
-                              return Image.network(
-                                itemModel.imageDownUrl[index],
-                                fit: BoxFit.cover,
-                                scale: 0.1,
-                              );
-                            },
-                            itemCount: itemModel.imageDownUrl.length,
+                _size = MediaQuery.of(context).size;
+                _statusBarHeight = MediaQuery.of(context).padding.top;
+                return Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Scaffold(
+                      body: CustomScrollView(
+                        controller: _scrollController,
+                        slivers: [
+                          SliverAppBar(
+                            expandedHeight: _size!.width,
+                            pinned: true,
+                            flexibleSpace: FlexibleSpaceBar(
+                              centerTitle: true,
+                              title: SmoothPageIndicator(
+                                  controller: _pageController, // PageController
+                                  count: itemModel.imageDownUrl.length,
+                                  effect: WormEffect(
+                                      activeDotColor: Theme.of(context)
+                                          .primaryColor,
+                                  dotHeight: 10,
+                                  dotWidth: 10), // your preferred effect
+                                  onDotClicked: (index) {}),
+                              background: PageView.builder(
+                                controller: _pageController,
+                                allowImplicitScrolling: true,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Image.network(
+                                    itemModel.imageDownUrl[index],
+                                    fit: BoxFit.cover,
+                                    scale: 0.1,
+                                  );
+                                },
+                                itemCount: itemModel.imageDownUrl.length,
+                              ),
+                            ),
                           ),
+                          SliverToBoxAdapter(
+                            child: Container(
+                              height: _size!.height * 2,
+                              color: Colors.pinkAccent,
+                              child: Center(child: Text('myKey ${widget.itemKey}')),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      left: 0,right: 0,top: 0,height: kToolbarHeight+_statusBarHeight!,
+                      child: Scaffold(
+                        backgroundColor: Colors.transparent,
+                        appBar: AppBar(
+                          backgroundColor: isAppbarCollapsed ? Colors.white: Colors.transparent,
+                          foregroundColor: isAppbarCollapsed ? Colors.black87 : Colors.white,
+                          shadowColor: Colors.transparent,
                         ),
                       ),
-                      SliverToBoxAdapter(
-                        child: Container(
-                          height: _size.height * 2,
-                          color: Colors.pinkAccent,
-                          child: Center(child: Text('myKey ${widget.itemKey}')),
-                        ),
-                      ),
-                    ],
-                  ),
+                    )
+                  ],
                 );
               },
             );
